c911b3558153235d9dfd686b5eacbd3b2b11eb5a
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-12-16 16:14:47 +0200

    1 stage

---
+++ js fundamentals 1 part/starter/assignment.js
@@ -0,0 +1,7 @@
+let country = 'Ukraine'
+let continent = 'Europe'
+let population = 40000000
+
+console.log(country)
+console.log(continent)
+console.log(population)
---
+++ js fundamentals 1 part/starter/script.js
@@ -0,0 +1,28 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+
+console.log("Dima");
+console.log(23);
+
+let firstName = "Matilda";
+let first = 'jonas';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let name_dima = 'dima'; //can consist _
+let $function = 'dima'; //can consist $
+
+let Person = 'dima'; //for OOP
+let pi = 3.1415; //for Constraints
+
+let myFirstJob = 'Programmer';
+myCurrentJob = 'Teacher';
+
+//variable - box, that we put value in
+
+console.log(myFirstJob)
+
+// Value types - number, string, boolean, undefined(empty value), null(empty value), symbol(unique), bigInt(larger integers)

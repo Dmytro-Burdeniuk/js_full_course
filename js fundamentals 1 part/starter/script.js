8ff8e4a534d1cf8235300c09492be21ac3f0a62e
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-12-16 16:50:38 +0200

    value types

--- js fundamentals 1 part/starter/script.js
+++ js fundamentals 1 part/starter/script.js
@@ -1,28 +1,46 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
 
-console.log("Dima");
-console.log(23);
+// console.log("Dima");
+// console.log(23);
 
-let firstName = "Matilda";
-let first = 'jonas';
+// let firstName = "Matilda";
+// let first = 'jonas';
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let name_dima = 'dima'; //can consist _
-let $function = 'dima'; //can consist $
+// let name_dima = 'dima'; //can consist _
+// let $function = 'dima'; //can consist $
 
-let Person = 'dima'; //for OOP
-let pi = 3.1415; //for Constraints
+// let Person = 'dima'; //for OOP
+// let pi = 3.1415; //for Constraints
 
-let myFirstJob = 'Programmer';
-myCurrentJob = 'Teacher';
+// let myFirstJob = 'Programmer';
+// myCurrentJob = 'Teacher';
 
-//variable - box, that we put value in
+// //variable - box, that we put value in
 
-console.log(myFirstJob)
+// console.log(myFirstJob)
+
 
 // Value types - number, string, boolean, undefined(empty value), null(empty value), symbol(unique), bigInt(larger integers)
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'Dima');
+
+// javascriptIsFun = 'Yes';
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null)

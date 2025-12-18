41a9f432b9e0afce9ece0cf4407fe576b3975a37
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-12-18 23:03:20 +0200

    first project with DOM

--- 5dom/script.js
+++ 5dom/script.js
@@ -1,12 +1,70 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+const DEFAULT_SCORE = 20;
+const DEFAULT_BACKGROUND_COLOR = '#222';
+const DEFAULT_NUMBER_WIDTH = '15rem'
+const DEFAULT_MESSAGE = 'Start guessing...'
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = DEFAULT_SCORE;
+let highscrore = 0; 
+let symbol = document.querySelector('.number').textContent;
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message
+}
+
+function logicGame () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+
+    // When no input
+    displayMessage('⛔️ No number');
+
+    // When player wins 
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 Correct Number');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem'
+
+    if (score > highscrore) {
+      highscrore = score;
+      document.querySelector('.highscore').textContent = highscrore
+
+    }
+  }
+    //When guess is wrong 
+    else if (guess !== secretNumber) {
+        if (score > 1) {
+        displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low';)
+        score--;
+        document.querySelector('.score').textContent = score;
+
+        // When player lost
+      } else {
+    displayMessage('😭 You lost');
+        document.querySelector('.score').textContent = 0;
+      }
+    }
+}
+
+function newGame () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = DEFAULT_SCORE;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = symbol;
+  document.querySelector('body').style.backgroundColor = DEFAULT_BACKGROUND_COLOR;
+  document.querySelector('.number').style.width = DEFAULT_NUMBER_WIDTH;
+  displayMessage(DEFAULT_MESSAGE);
+  document.querySelector('.guess').value = '';
+}
+
+document.querySelector('.check').addEventListener('click', logicGame);
+document.querySelector('.again').addEventListener('click', newGame);
+
+newGame()
 
 
-document.querySelector('.guess').value = 23
-console.log(document.querySelector('.guess').value); 

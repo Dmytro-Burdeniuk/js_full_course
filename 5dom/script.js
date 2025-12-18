4e71c201298ee67359730826c73ada1b90201745
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-12-18 23:03:35 +0200

    fix

--- 5dom/script.js
+++ 5dom/script.js
@@ -39,7 +39,7 @@ function logicGame () {
     //When guess is wrong 
     else if (guess !== secretNumber) {
         if (score > 1) {
-        displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low';)
+        displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low');
         score--;
         document.querySelector('.score').textContent = score;
 

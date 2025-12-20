f79924f0ff2507379cad37c07839bed0f90b1689
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-12-21 00:17:32 +0200

    global scoping

--- 8behindscenes/script.js
+++ 8behindscenes/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+ 
+function calcAge (birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge () {
+        const output = `You are ${age}, born in ${birthYear}`
+        console.log(output);
+    }
+
+    printAge()
+
+    return age;
+}
+
+const firstName = 'Dima'
+calcAge(1991);

b2ac8b242e95d59034c4282480b88a8349f079c6
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-12-21 02:45:11 +0200

    hoisting practice

--- 8behindscenes/script.js
+++ 8behindscenes/script.js
@@ -1,37 +1,79 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const firstName = 'Steven';
-      const str = `Oh, you are millenial ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = 'Steven';
+//       const str = `Oh, you are millenial ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-      output = 'NEW OUTPUT'
-    }
+//       output = 'NEW OUTPUT'
+//     }
 
-    // console.log(str);
-    console.log(millenial);
+//     // console.log(str);
+//     console.log(millenial);
 
-    console.log(output);
+//     console.log(output);
 
-    // console.log(add(2, 3));
-  }
+//     // console.log(add(2, 3));
+//   }
 
-  printAge();
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = 'Dima';
+// calcAge(1991);
+
+// Hoisting with variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Dima';
+// let job = 'teacher';
+// const year = 2004;
+
+// console.log(addDecl(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+// // Functions
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
 }
 
-const firstName = 'Dima';
-calcAge(1991);
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);

b07a2c021980f5fa307d9ecd40321f45ac57fe9c
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-12-26 02:14:46 +0200

    closures v2

--- 10functions/script.js
+++ 10functions/script.js
@@ -218,20 +218,61 @@
 
 // console.log(notPrivate);
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++
-    console.log(passengerCount);
-  }
-}
+//   return function () {
+//     passengerCount++
+//     console.log(passengerCount);
+//   }
+// }
 
-const booker = secureBooking();
+// const booker = secureBooking();
 
-booker();
-booker();
-booker();
-booker();
-booker();
-booker();
+// booker();
+// booker();
+// booker();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker)
+
+// Example 1
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// h();
+// f();
+
+// console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  // const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passngers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait}`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
